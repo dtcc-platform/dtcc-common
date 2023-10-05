d33605932500a88b3da94581881269e3fdf75aa7
Anders Logg <logg@chalmers.se>
2023-10-05 12:09:30 +0200

    Make error and critical print + exit

--- src/dtcc_common/dtcc_logging.py
+++ src/dtcc_common/dtcc_logging.py
@@ -3,24 +3,36 @@
 
 import logging as _logging
 
+
 def _init_logging(name):
     "Internal function for initializing logging"
+
+    # Initialize logger
     format = "%(asctime)s [%(name)s] [%(levelname)s] %(message)s"
     _logging.basicConfig(format=format)
     logger = _logging.getLogger(name)
-    logger.setLevel(_logging.INFO)    
-    return (logger.debug,
-            logger.info,
-            logger.warning,
-            logger.error,
-            logger.critical)
+    logger.setLevel(_logging.INFO)
+
+    # Define error and critical as print + exit
+    def error(message):
+        logger.error(message)
+        exit(1)
+
+    def critical(message):
+        logger.critical(message)
+        exit(1)
+
+    return (logger.debug, logger.info, logger.warning, error, critical)
+
 
 debug, info, warning, error, critical = _init_logging("dtcc-common")
 
+
 def init_logging(name):
-    "Initialize logging for given package"    
+    "Initialize logging for given package"
     debug(f"Initializing logging for {name}")
     return _init_logging(name)
+
 
 def set_log_level(level):
     """Set log level. Valid levels are:
